cb75d1ac641609d9a44cf9f1a991c360410a2bd0
shafin321 <shafinhaque91@gmail.com>
2020-12-20 03:39:05 +0100

    implemetn user

---
+++ UserDemo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UserDemo.Data;
+
+[assembly: HostingStartup(typeof(UserDemo.Areas.Identity.IdentityHostingStartup))]
+namespace UserDemo.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
